0348f3dd8c8eab428805d3f144a8a42e0c9b9fd2
Mohammed Risal <94342524+Devriz1@users.noreply.github.com>
2025-05-13 22:28:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+ var typed = new Typed("#typed", {
+    strings: ["We're working hard to bring you an amazing learning experience. Stay tuned for our launch where you can explore our Spoken English, TTC, and Soft Skills courses."],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: false
+  });
+
